1aec258f880bcdd7e4ee7c5aa0d2f47f286966de
Nora <105034161+Nora-George@users.noreply.github.com>
2024-05-16 17:16:35 -0600

    Practica de Lector de Codigo

---
+++ LectorQR/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace LectorQR
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnScanClicked(object sender, EventArgs e)
+        {
+            var options = new ZXing.Mobile.MobileBarcodeScanningOptions();
+            var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+
+            var result = await scanner.Scan(options);
+
+            if (result != null)
+            {
+               
+                await DisplayAlert("Resultado", result.Text, "OK");
+            }
+        }
+
+        private void OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                if (result != null && !string.IsNullOrEmpty(result.Text))
+                {
+                
+                    await DisplayAlert("Resultado", result.Text, "OK");
+                }
+            });
+        }
+    }
+}
